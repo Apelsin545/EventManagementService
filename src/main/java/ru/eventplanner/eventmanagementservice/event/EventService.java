f29948191fc1ea2743c42a02c4de3276c2e9a1af
Dmitrii <dmitri_ch@list.ru>
2024-07-21 19:43:36 +0300

    feat: создать EventService, заинжектить EventRepository

---
+++ src/main/java/ru/eventplanner/eventmanagementservice/event/EventService.java
@@ -0,0 +1,14 @@
+package ru.eventplanner.eventmanagementservice.event;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+    private final EventRepository eventRepository;
+
+    @Autowired
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+}
