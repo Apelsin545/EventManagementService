422365dcea944af82be4fb1fb7cde62bd24b621d
Dmitrii <dmitri_ch@list.ru>
2024-07-21 19:38:51 +0300

    Добавить класс составного ключа ParticipantPK

---
+++ src/main/java/ru/eventplanner/eventmanagementservice/participant/ParticipantPK.java
@@ -0,0 +1,13 @@
+package ru.eventplanner.eventmanagementservice.participant;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class ParticipantPK implements Serializable {
+    private Long eventId;
+    private String userId;
+}
